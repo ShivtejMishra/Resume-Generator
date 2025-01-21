3459b951d9e86a1e4782aeec7a4b0db0faa20d97
Shiv <mishrashivtej54@gmail.com>
2025-01-21 17:57:12 +0530

    Update ai_enhancement.py

--- ai_enhancement.py
+++ ai_enhancement.py
@@ -1,9 +1,16 @@
-# ai_enhancement.py
 from langchain_google_genai.llms import GoogleGenerativeAI
 from langchain import PromptTemplate, LLMChain
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API key from environment variables
+api_key = os.getenv("GOOGLE_API_KEY")
 
 # Initialize the GoogleGenerativeAI LLM
-llm = GoogleGenerativeAI(model="gemini-pro", api_key="AIzaSyDFK-Rw9JyG4yEejzqJWw0bTUmO1Vn3KaI")
+llm = GoogleGenerativeAI(model="gemini-pro", api_key=api_key)
 
 def enhance_summary(summary):
     prompt = PromptTemplate(
